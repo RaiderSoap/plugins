de47359f7d63734ef5ac78ec52c8778fa38fb96a
Raider Soap <info@chivalrystudio.com>
2016-08-24 05:11:31 -0700

    [Stage 1] done

--- Game_CoreSystem.js
+++ Game_CoreSystem.js
@@ -29,13 +29,13 @@ Game_Object.prototype.initialize = function(objectId,x,y) {
     
     
     this._x = x; 
-    this._y = y; 
+    this._y = y - 1; 
     this._width = data.width;
     this._height = data.height;
 
     this._passableGrids = data.passableGrids; //the size of this array should be width*height (in 32)
                               //and it should store data like 0101, 1111, 1010 binary numbers
-        //Alert, need to be fix
+        // need to be fix
 
     this._blendMode = 0;
     this._through = false;
@@ -48,13 +48,10 @@ Game_Object.prototype.characterName = function() {
 
 
 Game_Object.prototype.indexToMapIndex = function(index){
-    //return (x-this._x) + (y-this._y)*(this._height);
     var x = this._x + index % this._width;
     var y = this._y + index / this._width;
     y = ~~y;
-    //console.log("x: "+x+", y: "+y);
     return x+ y*$dataMap.width;
-    //console.log("x: "+x+", y: "+y+", index: "+);
 };
 
 Game_Object.prototype.screenX = function() {
@@ -80,12 +77,11 @@ Game_Object.prototype.screenY = function() {
      * 9 : Destination
      */
 Game_Object.prototype.screenZ = function() {
-    //return this._priorityType * 2 + 1;
     return 4;
 };
 Game_Object.prototype.scrolledX = function() {
-    var shifter = this._width % 2 === 0 ? 0.5 : 1;
-    return $gameMap.adjustX(this._x - shifter);
+    //var shifter = this._width % 2 === 0 ? 0.5 : 1;
+    return $gameMap.adjustX(this._x - 0.5);
 };
 
 Game_Object.prototype.scrolledY = function() {
@@ -107,7 +103,7 @@ Game_Map.prototype.setup = function(mapId) {
     
     this._objectsLayer = new Array(this.width()*this.height());
     for (var i = this._objectsLayer.length - 1; i >= 0; i--) {
-        this._objectsLayer[i] = 0;
+        this._objectsLayer[i] = 15;
     }
 
     this.setupObjects();
@@ -129,8 +125,13 @@ Game_Map.prototype.setupObjects = function() {
         if (event.list()[0].code !== 108) {
             return;
         } 
-        if (event.list()[0].parameters[0] === "Tree") {
-            self._objects.push(new Game_Object(1,event.x,event.y));
+        if (event.list()[0].parameters[0] === "Object") {
+            if (event.list()[1].code !== 408) {
+                return;
+            }
+            var pattern = /\d+/;
+            var objID =    Number(event.list()[1].parameters[0].match(pattern));
+            self._objects.push(new Game_Object(objID,event.x,event.y));
             event.erase();
         };
         
@@ -146,15 +147,19 @@ Game_Map.prototype.setupObjectsLayer = function() {
     });
 
     
-    
 };
 
 Game_CoreSystem.Core.Game_Map_isPassable = Game_Map.prototype.isPassable;
 Game_Map.prototype.isPassable = function(x, y, d) {
     var width = $dataMap.width;
-    return !this._objectsLayer[x+width*y] && 
+    return this.checkObjectPassage(x,y,d) && 
         Game_CoreSystem.Core.Game_Map_isPassable.call(this,x, y, d);
 };
+Game_Map.prototype.checkObjectPassage = function(x,y,d) {
+    var width = $dataMap.width;
+    return this._objectsLayer[x+width*y] & (1 << (d / 2 - 1)) ;
+};
+
 //-----------------------------------------------------------------------------
 // Sprite_Object
 //---------------------------------------------------------------------------- 
